0f980fd269659c634dd6e4ef3e1586c04b90e2a3
ektrac <36294262+ektrac@users.noreply.github.com>
2026-01-11 14:22:30 +0300

    Update yapsunu.py

--- yapsunu.py
+++ yapsunu.py
@@ -36,7 +36,6 @@ def get_stream_url(youtube_url):
     cmd = [
         "yt-dlp",
         "--js-runtimes", "node",  # Node.js runtime kullan
-        "--cookies", COOKIES_FILE, # buraya cookies parametresi eklendi
         "-f", "625+140/625+bestaudio/best[ext=mp4]/best",
         "-g", youtube_url
     ]
